8491b65b9642055dae014df8e65574083dbddaa8
fahim faysal <ffalsoy@gmail.com>
2025-07-06 02:01:47 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/vite-test-test/' : '/',
+  // base: process.env.NODE_ENV === 'production' ? '/vite-test-test/' : '/',
+  base: '/',
+
   // base: '/vite-test-test/',
   optimizeDeps: {
     exclude: ['lucide-react'],
